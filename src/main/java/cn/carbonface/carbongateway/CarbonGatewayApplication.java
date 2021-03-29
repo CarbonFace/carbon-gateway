7516a847e4ddcd21e1725494a5e458a296aacb5d
CarbonFace <553127022@qq.com>
2021-03-29 17:11:53 +0800

    rebase common
    2021-3-29 17:11:44  Xiaoyi Ye

--- src/main/java/cn/carbonface/carbongateway/CarbonGatewayApplication.java
+++ src/main/java/cn/carbonface/carbongateway/CarbonGatewayApplication.java
@@ -1,11 +1,19 @@
 package cn.carbonface.carbongateway;
 
+//import cn.carbonface.carbongateway.security.config.JWTConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
+//@EnableConfigurationProperties({JWTConfig.class})
+@ComponentScan("cn.carbonface")
+//@EnableFeignClients
 public class CarbonGatewayApplication {
 
     public static void main(String[] args) {
