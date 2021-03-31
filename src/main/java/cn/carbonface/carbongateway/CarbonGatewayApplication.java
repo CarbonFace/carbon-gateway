e7e0f1b0f5f856d6b0f792dc07921c590bfec5cd
CarbonFace <553127022@qq.com>
2021-03-31 18:32:05 +0800

    1、carbon-security module introduced 2、spring-boot-starter-parent version updated
    2021-3-31 13:55:24  Xiaoyi Ye

--- src/main/java/cn/carbonface/carbongateway/CarbonGatewayApplication.java
+++ src/main/java/cn/carbonface/carbongateway/CarbonGatewayApplication.java
@@ -1,19 +1,13 @@
 package cn.carbonface.carbongateway;
 
-//import cn.carbonface.carbongateway.security.config.JWTConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication/*(exclude = DataSourceAutoConfiguration.class)*/
+@SpringBootApplication
 @EnableDiscoveryClient
-//@EnableConfigurationProperties({JWTConfig.class})
 @ComponentScan("cn.carbonface")
-//@EnableFeignClients
 public class CarbonGatewayApplication {
 
     public static void main(String[] args) {
