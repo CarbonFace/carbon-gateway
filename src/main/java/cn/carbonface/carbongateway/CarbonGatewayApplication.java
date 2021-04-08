8800cc4891a10589938092562f570332202a2765
CarbonFace <553127022@qq.com>
2021-04-08 18:30:04 +0800

    1、update the whole project structure 2、update the security module which introduced @FeignOnly,@NoAuth annotations for feign client invokes and no authorization controllers 3、introduced the SecurityRedisUtil which is isolate with the common redis util to manage the token information alone 4、move the security related codes into security module 5、switch the redis config properties into lettuce
    2021-4-8 18:29:24  Xiaoyi Ye

--- src/main/java/cn/carbonface/carbongateway/CarbonGatewayApplication.java
+++ src/main/java/cn/carbonface/carbongateway/CarbonGatewayApplication.java
@@ -3,11 +3,9 @@ package cn.carbonface.carbongateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan("cn.carbonface")
 public class CarbonGatewayApplication {
 
     public static void main(String[] args) {
